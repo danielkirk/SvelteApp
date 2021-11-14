4c3505d845b6a7c4457940fcc6d6be2e2d2101aa
Daniel Kirk <imdanielkirk@gmail.com>
2021-11-13 18:02:18 -0800

    completed bar chart component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
 	import Header from "./UI/Header.svelte";
+	import Controls from "./UI/Controls.svelte";
 </script>
 
 <Header />
+<br />
+<Controls />
---
+++ src/UI/Controls.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Chart from './Chart.svelte';
+
+    let data = [1,2,3,4,5,6];
+</script>
+<br />
+<br />
+
+<input type="number"  bind:value={data[0]}/>
+<input type="number"  bind:value={data[1]}/>
+<input type="number"  bind:value={data[2]}/>
+<input type="number"  bind:value={data[3]}/>
+<input type="number"  bind:value={data[4]}/>
+<input type="number"  bind:value={data[5]}/>
+
+<Chart {data} />
